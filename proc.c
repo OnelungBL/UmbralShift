a85a49cf40ddb355e73f4aac17d5f7549c2da370
OnelungBL <onelung19@yahoo.com>
2016-02-18 20:42:47 -0800

    Update proc.c

--- proc.c
+++ proc.c
@@ -16,7 +16,7 @@ void LoadRun() {         // this is not real
 void IdleProc() {
    int i;
    
-   cons_printf("IdleProc (PID 0) runs.\n", IDT_ptr);
+   cons_printf("IdleProc (PID %d) runs.\n", IDT_ptr);
    for(i=0; i<1666667; i++) {
       IO_DELAY();
    }

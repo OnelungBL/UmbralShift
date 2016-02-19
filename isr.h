ac26feafb0af0ed7be1f66eeb9173b2119c95ea8
OnelungBL <onelung19@yahoo.com>
2016-02-18 22:28:42 -0800

    Update isr.h

--- isr.h
+++ isr.h
@@ -3,8 +3,8 @@
 #ifndef _ISR_H_
 #define _ISR_H_
 
-void StartProcISR(int, q_t *);
-void EndProcISR(int, q_t *);
-void TimerISR(int, q_t *);
+void StartProcISR(int, *q_t);
+void EndProcISR(int, *q_t);
+void TimerISR(int, *q_t);
 
 #endif

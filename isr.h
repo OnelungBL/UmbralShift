c3cad92e763ef4a2bb59df875b60ba81198092a6
OnelungBL <onelung19@yahoo.com>
2016-02-20 15:46:28 -0800

    Added files via upload

---
+++ isr.h
@@ -0,0 +1,12 @@
+// isr.h, 159
+
+#ifndef _ISR_H_
+#define _ISR_H_
+
+#include "typedef.h" // q_t needs be defined in code below
+
+void StartProcISR(int, q_t *, pcb_t *);
+void EndProcISR(int *, q_t *, pcb_t *);
+void TimerISR(int *, q_t *, pcb_t *);
+
+#endif
---
+++ proc.c
@@ -0,0 +1,32 @@
+// proc.c, 159
+// processes are coded here
+
+#include "spede.h"      // for IO_DELAY() needed here below
+#include "extern.h"     // for currently-running pid needed below
+#include "proc.h"       // for prototypes of process functions
+
+void LoadRun(int running_pid) {         // this is not real
+   if (running_pid == 0) {
+      IdleProc();
+   } else {
+      UserProc(running_pid);
+   }
+}
+
+void IdleProc() {
+   int i;
+   
+   cons_printf("IdleProc (PID 0) runs.\n");
+   for(i=0; i<1666667; i++) {
+      IO_DELAY();
+   }
+}
+
+void UserProc(int running_pid) {
+   int i;
+   
+   cons_printf("UserProc (PID %d) runs.\n", running_pid);
+   for(i=0; i<1666667; i++) {
+      IO_DELAY();
+   }
+}

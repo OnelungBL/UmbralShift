4e0f7195f3038e9406c4f7a5e5fd27463f31616d
OnelungBL <onelung19@yahoo.com>
2016-02-21 13:58:22 -0800

    Update main.c

--- main.c
+++ main.c
@@ -85,6 +85,7 @@ void KernelMain(TF_t *TF_ptr) {
   switch(TF_ptr->intr_id) {
   	case TIMER_INTR:
   		TimerISR(&running_pid, &ready_q, pcb); //dismiss timer event: send PIC with a code
+  		outportb(0x20, 0x60);
   		break;
   	default:
   		cons_printf("Panic: unknown intr ID(%d)!\n", TF_ptr->intr_id);

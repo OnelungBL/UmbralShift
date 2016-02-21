39abbc28225bc32a37e2abd6a8517c302af48425
OnelungBL <onelung19@yahoo.com>
2016-02-21 15:17:58 -0800

    Update isr.c

--- isr.c
+++ isr.c
@@ -67,7 +67,6 @@ void TimerISR(q_t *ready_q, pcb_t *pcb) {
    //in PCB, upcount both runtime and total_runtime of running process
    pcb[running_pid].runtime++;
    pcb[running_pid].total_runtime++;
-   printf("running pid: %d runtime: %d, total runtime: %d\n", running_pid, pcb[running_pid].runtime, pcb[running_pid].total_runtime);
    if (pcb[running_pid].runtime >= TIME_LIMIT) {
    	pcb[running_pid].runtime = 0;
    	pcb[running_pid].state=READY;
